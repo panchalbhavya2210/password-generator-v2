7ef753adf3c87b692e41a9b0d9ba89a9e09fff92
BHAVYA\bhavy <dev.pbhavya@gmail.com>
2026-02-16 16:49:14 +0530

    Add nocheck

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,4 @@
-// @ts-ignore
+// @ts-nocheck
 import SectorTable from "./components/fii/sector-table";
 import { sectorColumns } from "./components/fii/sector-columns";
 import { getCDSLData } from "../lib/cdsl";
